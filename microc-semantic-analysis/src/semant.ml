db610b0a96012c6fa16edd43dabef2603aa8396e
piazzesiNiccolo <npiazzesi@gmail.com>
2021-04-22 10:58:21 +0200

    added variable check

--- microc-semantic-analysis/src/semant.ml
+++ microc-semantic-analysis/src/semant.ml
@@ -5,25 +5,55 @@ type pos = Lexing.position * Lexing.position
 type var_info = pos * typ
 type fun_info = 
   (* we keep builtins functions implemented externally separated from normal
-  function declararions *)
-    | External of typ * typ list 
-    | Declaration of pos*fun_decl
+     function declararions *)
+  | External of typ * typ list 
+  | Declaration of pos*fun_decl
 
 type symbols = {fun_symbols: fun_info Symbol_table.t ; 
                 var_symbols: var_info Symbol_table.t
-                }
+               }
 
 let check_func f scope loc =
   try
     Symbol_table.add_entry f.fname (Declaration(loc, f)) scope.fun_symbols |> ignore 
-  with 
-    Symbol_table.DuplicateEntry ->  "function "^ f.fname ^ " already defined" |> Util.raise_semantic_error loc 
-  
+  with Symbol_table.DuplicateEntry ->
+    Util.raise_semantic_error loc @@    "function "^ f.fname ^ " already defined"
+
+let check_type loc t = 
+  match t with
+  | TypA(TypA(_,_),_) -> Util.raise_semantic_error loc "Cannot define multidimensional array"
+  | TypA(TypV,_) -> Util.raise_semantic_error loc "Trying to define a void array"
+  | TypA(t,Some i) when i < 1 -> Util.raise_semantic_error loc  "Array must have size > 0"
+  | TypA(t, None) -> Util.raise_semantic_error loc "Array must be declared with an initial size"
+  | TypP(TypV) -> Util.raise_semantic_error loc "Trying to define a void pointer"
+  | _ -> ()
+
+let check_var_type loc t = 
+  match t with
+  | TypV -> Util.raise_semantic_error loc "Trying to define a void variable"
+  | _ -> check_type loc t
+
+let check_var_decl (t,i) scope loc =
+  let check_var_type loc t = 
+    match t with
+    | TypV -> Util.raise_semantic_error loc "Trying to define a void variable"
+    | _ -> check_type loc t
+  in
+  check_var_type loc t;
+  try 
+    Symbol_table.add_entry i (loc, t) scope.var_symbols |> ignore
+  with DuplicateEntry ->
+    Util.raise_semantic_error loc @@ "Variable" ^ i ^ " already defined in current scope"
+
+
 let check_topdecl scope node = 
   match node.node with
   |Fundecl(f) ->  check_func f scope node.loc
-                
-  |Vardec(t,i) -> Symbol_table.add_entry i (node.loc, t) scope.var_symbols |> ignore
+
+  |Vardec(t,i) -> check_var_decl (t,i) scope node.loc
+
+
+
 
 let dummy = Lexing.dummy_pos, Lexing.dummy_pos
 let check_global_properties scope =
@@ -34,14 +64,18 @@ let check_global_properties scope =
   | Some _ -> Util.raise_semantic_error dummy "Invalid signature of main"
   | None -> Util.raise_semantic_error dummy " No main function defined"
 
-  let prelude_functions =
+
+
+let prelude_functions =
   let init_scope = Symbol_table.empty_table |> Symbol_table.begin_block in 
   List.iter (fun (name, rt, p) -> Symbol_table.add_entry name (External(rt, p)) init_scope |> ignore) Util.prelude;
   init_scope
 
-  let check (Prog(topdecls)) = 
-      let toplevel_scope = {
-        fun_symbols = prelude_functions;
-        var_symbols = Symbol_table.empty_table |> Symbol_table.begin_block
-      }
-      in List.iter (check_topdecl toplevel_scope) topdecls;check_global_properties toplevel_scope |> ignore 
+
+
+let check (Prog(topdecls)) = 
+  let toplevel_scope = {
+    fun_symbols = prelude_functions;
+    var_symbols = Symbol_table.empty_table |> Symbol_table.begin_block
+  }
+  in List.iter (check_topdecl toplevel_scope) topdecls;check_global_properties toplevel_scope |> ignore 
